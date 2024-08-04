fc58514cdc92ca3b5b821560192ca03e63b9e449
Ɗყηᴀʍɨᴄ ◠‿◕ <adarshpanditdev@gmail.com>
2024-08-04 20:04:39 +0530

    Home Page -complete

---
+++ ui/global/nav/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.scss';
+
+function Navbar() {
+  return (
+    <div className={styles.nav}>
+        <h1>LOGO</h1>
+    </div>
+  )
+}
+
+export default Navbar
